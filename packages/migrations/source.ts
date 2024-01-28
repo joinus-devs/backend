0dc047334d87d7901ca186ba258de7c6eb25ec9b
Lee Minhoon <hackids@naver.com>
2024-01-28 22:11:15 +0900

    fixed db connection

--- packages/migrations/source.ts
+++ packages/migrations/source.ts
@@ -9,8 +9,8 @@ dotenv.config();
 
 export default new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: +process.env.DB_HOST!,
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT!,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
